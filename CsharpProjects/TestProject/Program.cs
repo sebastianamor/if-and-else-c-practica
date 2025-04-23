cf9aa0354c674d5dd8db548b5037d738bd9ca39d
sebastian lapt <smoreira11@hotmail.com>
2025-04-23 11:56:10 +0900

    desafio if and else en C

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,30 +1,15 @@
-﻿Random dice = new Random();
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// Your code goes here
 
-int total = roll1 + roll2 + roll3;
+int total = daysUntilExpiration + discountPercentage;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+Console.WriteLine($"Dice roll:  {total}");
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
 
-    Console.WriteLine($"Your total including the bonus: {total}");
-}
-
-if (total >= 16)
+if (total >= 12)
 {
     Console.WriteLine("You win a new car!");
 }
